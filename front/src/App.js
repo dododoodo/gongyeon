905083bc468f5b6bb2aedd612313917be899aab3
Doyeon Kim <nsa10050@naver.com>
2025-04-22 16:37:14 +0900

    배경수정6

--- front/src/App.js
+++ front/src/App.js
@@ -11,16 +11,13 @@ function App() {
         <div className="left">
           <p>예술의 전당</p>
         </div>
-
         <div className="right">
           <p>공연을 한눈에!</p>
         </div>
-
         <img src={logo} alt="공연본당 로고 이미지" />
-
-        <div className="container">
+      </div>
+      <div className="container">
           <AppRouter />
-        </div>
       </div>
     </BrowserRouter>
   );
