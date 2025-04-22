bdbf3bbbb7df4bb178a9f4816c4e30b825926940
Doyeon Kim <nsa10050@naver.com>
2025-04-22 16:18:22 +0900

    배경수정2

--- front/src/App.js
+++ front/src/App.js
@@ -7,14 +7,14 @@ import logo from './images/home_preview_02.png';
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="all_container">
         <div className="left">예술의 전당</div>
         <div className="right">공연을 한눈에!</div>
         <img src={logo} alt="공연본당 로고" />
-      </div>
 
-      <div className="container">
-        <AppRouter />
+        <div className="container">
+          <AppRouter />
+        </div>
       </div>
     </BrowserRouter>
   );
