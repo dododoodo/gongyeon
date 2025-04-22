d37f78edb51bbde3b82473d534bcd8de12366942
Doyeon Kim <nsa10050@naver.com>
2025-04-22 16:01:58 +0900

    끝4

--- front/src/App.js
+++ front/src/App.js
@@ -2,11 +2,16 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/Router';
 import './styles/main.scss';
 import './styles/viewport.scss';
-
+import logo from './images/home_preview_02.png'; 
 
 function App() {
   return (
     <BrowserRouter>
+      <div className="bg_title">
+        <img src={logo} alt="공연본당 로고" />
+        <div>예술의 전당을 한눈에!</div>
+        <div className="slogan">공연본당</div>
+      </div>
       <div className="container">
         <AppRouter />
       </div>
