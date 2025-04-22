dbf47f1b81d80945f15e247bba9680f29176a326
Doyeon Kim <nsa10050@naver.com>
2025-04-22 17:31:07 +0900

    배경수정7

--- front/src/App.js
+++ front/src/App.js
@@ -2,22 +2,15 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/Router';
 import './styles/main.scss';
 import './styles/viewport.scss';
-import logo from './images/home_preview_03.png'; 
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="all_container">
-        <div className="left">
-          <p>예술의 전당-</p>
-        </div>
-        <div className="right">
-          <p>-공연을 한눈에!</p>
-        </div>
-        <img src={logo} alt="공연본당 로고 이미지" />
-      </div>
-      <div className="container">
+        <div className="container">
           <AppRouter />
+        </div>
       </div>
     </BrowserRouter>
   );
