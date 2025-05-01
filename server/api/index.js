8b1897f3c063345c06cd4c60e951753608918a5f
Doyeon Kim <nsa10050@naver.com>
2025-05-02 00:01:24 +0900

    수정

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,6 @@ const app = express();
 const bodyParser = require('body-parser');
 const kakao = require('./kakao');
 const naver = require('./naver');
-const kcisa = require('./kcisa');
 
 // CORS 설정
 app.use(cors({
@@ -19,6 +18,5 @@ app.use(bodyParser.json());
 
 app.use('/kakao', kakao);
 app.use('/naver', naver);
-app.use('/kcisa', kcisa);
 
 module.exports = app;
